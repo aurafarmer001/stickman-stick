8234b8226986af34c6c8aa4ee27e9a8be44c3823
Chan Yap Long <chan.yap.long@sji.edu.sg>
2025-08-02 09:51:19 +0800

    Initial Commit

---
+++ stickman sticky/stickman_stickyApp.swift
@@ -0,0 +1,17 @@
+//
+//  stickman_stickyApp.swift
+//  stickman sticky
+//
+//  Created by Chan Yap Long on 2/8/25.
+//
+
+import SwiftUI
+
+@main
+struct stickman_stickyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
